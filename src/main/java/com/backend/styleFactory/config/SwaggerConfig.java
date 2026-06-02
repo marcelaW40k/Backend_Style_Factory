aec1c7d6b7e496d2158fd552ceab1daa3f60f2eb
marcelaW40k <ardilamarcela42@gmail.com>
2026-06-01 19:18:57 -0500

    Agregando el OpenApi

--- src/main/java/com/backend/styleFactory/config/SwaggerConfig.java
+++ src/main/java/com/backend/styleFactory/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.backend.styleFactory.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "Style Factory API", version = "1.0"),security = @SecurityRequirement(name = "bearerAuth"))
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
